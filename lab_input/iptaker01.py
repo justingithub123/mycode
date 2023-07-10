480ecc046ada89dc991442c80cd59730846eed46
justingithub123 <justin.mckinley.hall@gmail.com>
2023-07-10 20:54:49 +0000

    mixed list

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,8 @@
+!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+print() - display data to std out"""
+
+def main():
+    user_input = input("Please enter an IPv4 IP address:")
+    print("You told me the IPv4 address is: " + user_input)
+main()
