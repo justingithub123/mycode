aa2fa652a44622b132e312d2e340199bbec55057
justingithub123 <justin.mckinley.hall@gmail.com>
2023-07-20 18:20:28 +0000

    Certification_proj_astronauts6

--- cert_proj/astronauts6.py
+++ cert_proj/astronauts6.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" application to track who is in space"""
+""" application to track who and how many in space"""
 
 import requests
 from crayons import blue, red
 
-def prompt_craft_choice():
+def prompt_craft_choice(): # prompt for which craft
     while True:
         craft_choice = input("Which craft would you like to see? Enter 1 for Craft 1 (ISS), 2 for Craft 2 (Tiangong), or 'q' to quit: ")
         if craft_choice.lower() == 'q':
             print("Goodbye")
-            exit()
+            exit() # end program upon q in prompt
         elif craft_choice == "1":
             return "ISS"
         elif craft_choice == "2":
             return "Tiangong"
-        else:
+        else: # error control
             print("Invalid input. Please select Craft 1 (ISS) or Craft 2 (Tiangong).")
 
-def main():
+def main(): # api pull to json and timeout (suggestion from pylint)
     try:
         astro = requests.get("http://api.open-notify.org/astros.json", timeout=30).json()
         astronauts = astro["people"]
@@ -25,7 +25,8 @@ def main():
     except requests.Timeout:
         print("Timeout occurred. Please try again later.")
         exit()
-
+   
+   # print number of astronauts in space
     print(f'There are {num_astronauts} people in space')
 
     craft_choice = prompt_craft_choice()
